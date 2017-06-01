b99dbf7de9bd96b2e1edee53e1d21c93a3f425c5
jnoortheen <jnoortheen@gmail.com>
2017-06-01 17:17:43 +0530

    feat: adding index page

--- newbie/settings.py
+++ newbie/settings.py
@@ -61,11 +61,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'newbie.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            # os.path.join(BASE_DIR, 'templates')
+            'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
